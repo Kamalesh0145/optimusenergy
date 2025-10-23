6ca614de07c54296d594ca7e4e6c08f20dea298c
Kamalesh0145 <kamalragu5@gmail.com>
2025-10-23 11:31:47 +0530

    Add files via upload

---
+++ consulting-website-template/contact.php
@@ -0,0 +1,72 @@
+<?php include 'includes/header.php'; ?>
+
+        <!-- Contact Start -->
+        <div class="contact mt-125">
+            <div class="container">
+                <div class="section-header">
+                    <p>Get In Touch</p>
+                    <h2>Get In Touch For Any Query</h2>
+                </div>
+                <div class="row align-items-center">
+                    <div class="col-md-5">
+                        <div class="contact-info">
+                            <div class="contact-icon">
+                                <i class="fa fa-map-marker-alt"></i>
+                            </div>
+                            <div class="contact-text">
+                                <h3>Our Head Office</h3>
+                                <p>12/94,Sathiyamoorthy Nagar,Thomas Cross Road,Opp.Central Warehouse,Ellis Nagar,Maduari-625016</p>
+                            </div>
+                        </div>
+                        <div class="contact-info">
+                            <div class="contact-icon">
+                                <i class="fa fa-phone-alt"></i>
+                            </div>
+                            <div class="contact-text">
+                                <h3>Call for Help</h3>
+                                <p>+91 99943 44816 </p>
+                            </div>
+                        </div>
+                        <div class="contact-info">
+                            <div class="contact-icon">
+                                <i class="fa fa-envelope"></i>
+                            </div>
+                            <div class="contact-text">
+                                <h3>Email for Information</h3>
+                                <p>admin@optimusenergy.in
+                                    technical@optimusenergy.in</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="contact-form">
+                            <div id="success"></div>
+                            <form name="sentMessage" id="contactForm" novalidate="novalidate">
+                                <div class="control-group">
+                                    <input type="text" class="form-control" id="name" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name" />
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="control-group">
+                                    <input type="email" class="form-control" id="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email" />
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="control-group">
+                                    <input type="text" class="form-control" id="subject" placeholder="Subject" required="required" data-validation-required-message="Please enter a subject" />
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="control-group">
+                                    <textarea class="form-control" id="message" placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div>
+                                    <button class="btn" type="submit" id="sendMessageButton">Send Message</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Contact End -->
+
+<?php include 'includes/footer.php'; ?>
